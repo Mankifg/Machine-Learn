9b7ec158a87e7b6758579355470f83d319f153f5
Mankifg <jaka.horvat2008@gmail.com>
2022-01-30 19:19:12 +0100

    add file

---
+++ decode.py
@@ -0,0 +1,54 @@
+
+
+
+what_i_have = []
+f = open("output.txt", "r")
+for x in f:
+  what_i_have.append(x)
+  
+
+codes = []
+values = []
+for i in range(len(what_i_have)):
+    one_line = what_i_have[i]
+    one_line = one_line.replace("\n", "")
+    a = one_line.split("|")
+    codes.append(a[0])
+    values.append(a[1])    
+    
+#print(codes)
+#print(values)
+
+yes = []
+no = []
+
+for z in range(len(codes)):
+    temp_code = codes[z]
+    if values[z] == '1':
+        #code is true
+        for x in range(len(temp_code)-1):
+            try_key = temp_code[x] + temp_code[x+1]
+            if not try_key in yes:
+                yes.append(try_key)
+    else:
+        for x in range(len(temp_code)-1):
+            try_key = temp_code[x] + temp_code[x+1]
+            if not try_key in yes:
+                no.append(try_key)
+                
+#print(yes)
+#print(no)
+
+ihope = []
+
+for d in range(len(yes)):
+    if not yes[d] in no:
+        ihope.append(yes[d])
+        
+        
+print("{} are possible keys".format(ihope))         
+        
+        
+
+
+            
---
+++ set.py
@@ -0,0 +1,40 @@
+from optparse import OptionGroup
+import random
+import string
+import time
+
+file = open("output.txt", "w")
+
+repeat = 50
+long  = 10
+#use = string.ascii_lowercase
+use = "abcde"
+output = []
+outoutput = []
+key = str(input('What is the key?'))
+
+file.write("")
+
+a = time.time()
+for a in range(repeat):
+    temp = ''
+    for i in range(long):
+        temp = temp + use[random.randint(0,len(use)-1)]
+    output.append(temp)
+    
+    
+    
+
+
+for s in range(len(output)):
+    if key in output[s]:
+        include = "1"
+    else:
+        include = "0"
+    end = output[s] + "|" + include
+    outoutput.append(end)
+    file.write(end)
+    file.write("\n")
+    
+print(output)
+print(outoutput)
